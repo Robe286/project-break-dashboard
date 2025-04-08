22fce6f8aec6ebdcb953251c349f1e4c83faf60b
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-08 11:33:11 +0200

    obtener valor numerico del i[Cnput

--- JS/key-Generator.js
+++ JS/key-Generator.js
@@ -1,4 +1,13 @@
 
-const characters = document.getElementById("characters")
-const password = parseInt(characters.value)
-console.log(password)
+
+
+const charactersInput = document.getElementById("charactersInput") //Nodo
+let charactersValue = 0
+    
+charactersInput.addEventListener('change', () =>{
+    charactersValue = parseInt(charactersInput.value)
+    if ( charactersValue < 12 || charactersValue > 50) {
+        alert("El valor introducido debe estar entre 12 y 50 caracteres.");
+        return;
+    }     
+})

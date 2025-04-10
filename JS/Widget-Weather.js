548fc3f17cc5e097a7262f0b4feef4c37586dbc0
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-10 11:55:51 +0200

    css current weather

--- JS/Widget-Weather.js
+++ JS/Widget-Weather.js
@@ -11,6 +11,7 @@ const getData = async () => {
             throw new Error ('Ha surgido un problema', response.status)
         }
         const weatherData = await response.json()
+        console.log(weatherData)
         const { name, country } = weatherData.location
         const { text, icon } = weatherData.current.condition  // Estado del clima y la imagen
         const { temp_c, humidity, precip_mm, wind_kph } = weatherData.current
@@ -18,7 +19,18 @@ const getData = async () => {
         currentWeather.innerHTML = `
         <h2>${name} - ${country }</h2>
         <p>${text}</p>
+        <div class="icon-temp">
         <img src="https:${icon}" alt="${text}">
+        <p>${temp_c}</p>
+        </div>
+        <ul>
+        <li>Viento: ${wind_kph}</li>
+        <li>Humedad: ${humidity}</li>
+        <li>Precipitaciones: ${precip_mm}</li>
+        </u>
+    
+        
+
         `
         
     }

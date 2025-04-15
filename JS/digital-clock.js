81a2d575129bd46d981637ef9e11d60773468f4c
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-15 11:30:34 +0200

    probando setInterval

--- JS/digital-clock.js
+++ JS/digital-clock.js
@@ -1,8 +1,15 @@
-const date = new Date()
-console.log(date)
+const getDate = new Date()
+console.log(getDate)
+// Se podrían meter las frases en variables, depués quizás meter en un array, en un objeto clave/valor y renderizarlo en el dom
 const MESES = [
     "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
     "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] 
-MESES[date.getMonth()]
-console.log(MESES[date.getMonth()])
-console.log(date.toLocaleDateString())
+MESES[getDate.getMonth()]
+console.log(MESES[getDate.getMonth()])
+console.log(getDate.toLocaleDateString())
+console.log(getDate.toLocaleTimeString())
+
+const date = getDate.toLocaleDateString()
+const time = getDate.toLocaleTimeString()
+console.log(date)
+console.log(time)

925f78c2eed16a19d23c357643d025e741224942
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-15 17:13:43 +0200

    conversión

--- JS/digital-clock.js
+++ JS/digital-clock.js
@@ -1,15 +1,62 @@
+const date = getDate.toLocaleDateString()
+// Obtenemos las horas, minutos y segundos. Devuelve un entero
+
+/*
+function activateClock () {
+    
+    
+}
+setInterval((activateClock()), 1000)
+*/
+
 const getDate = new Date()
 console.log(getDate)
+let hours = getDate.getHours();
+let minutes = getDate.getMinutes();
+let seconds = getDate.getSeconds();
+let time = '';
+// Convertimos los valores a dos dígitos
+hours = (hours <= 9) ? ('0' + hours) : hours;
+minutes = (minutes <= 9) ? ('0' + minutes) : hours;
+seconds = (seconds <= 9) ? ('0' + seconds) : seconds;
+
+time = hours + ':' + minutes + ':' + seconds;
+console.log(time)
+
+
+/*
+const clock = document.getElementById('clock')
+clock.innerHTML = `<span>${time}</span>`
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Se podrían meter las frases en variables, depués quizás meter en un array, en un objeto clave/valor y renderizarlo en el dom
 const MESES = [
     "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
     "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] 
 MESES[getDate.getMonth()]
-console.log(MESES[getDate.getMonth()])
-console.log(getDate.toLocaleDateString())
-console.log(getDate.toLocaleTimeString())
-
-const date = getDate.toLocaleDateString()
-const time = getDate.toLocaleTimeString()
-console.log(date)
-console.log(time)
+*/

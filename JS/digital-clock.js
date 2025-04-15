e6b2918296944a09a0e723d1ff2e899ae13a726a
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-15 10:50:08 +0200

    obtener date

--- JS/digital-clock.js
+++ JS/digital-clock.js
@@ -1,3 +1,8 @@
-const today = new Date(2024, 4, 14)
-console.log(today)
-
+const date = new Date()
+console.log(date)
+const MESES = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+    "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] 
+MESES[date.getMonth()]
+console.log(MESES[date.getMonth()])
+console.log(date.toLocaleDateString())
